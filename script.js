fab0cd8ce1395703b6b3b0dcf98c8b8c6abe0838
chanawh <thechanwh@gmail.com>
2018-08-23 23:32:34 -0700

    transitions

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let rockBtn = document.getElementById("rock");
 let paperBtn = document.getElementById("paper");
 let scissorsBtn = document.getElementById("scissors");
+let welcomeDiv = document.getElementById("welcome");
+let progressDiv = document.getElementById("progress");
 let resultsDiv = document.getElementById("results");
 let winnerDiv = document.getElementById("winner");
 let playerScore = 0;
@@ -23,46 +25,53 @@ function computerChoice(num) {
 }
 
 function evaluateRound(playerInput, computerInput) {
-  console.log("Computer chose " + computerInput + ".");
-
   if (playerInput === "rock" && computerInput === "scissors") {
-    console.log("Player wins, rock beats scissors!");
+    progressDiv.innerHTML =
+      "Your <b>ROCK</b> beats\nthe computer's <b>SCISSORS</b>!\n\nPlayer earns a point.";
     return playerScore++;
   } else if (playerInput === "paper" && computerInput === "rock") {
-    console.log("Player wins, paper beats rock!");
+    progressDiv.innerHTML =
+      "Your <b>PAPER</b> beats\nthe computer's <b>ROCK</b>!\n\nPlayer earns a point.";
     return playerScore++;
   } else if (playerInput === "scissors" && computerInput === "paper") {
-    console.log("Player wins, scissors beats paper!");
+    progressDiv.innerHTML =
+      "Your <b>SCISSORS</b> beats\nthe computer's <b>PAPER</b>!\n\nPlayer earns a point.";
     return playerScore++;
   } else if (computerInput === "rock" && playerInput === "scissors") {
-    console.log("Computer wins, rock beats scissors!");
+    progressDiv.innerHTML =
+      "The computer's <b>ROCK</b> beats\nyour <b>SCISSORS</b>!\n\nComputer earns a point.";
     return computerScore++;
   } else if (computerInput === "paper" && playerInput === "rock") {
-    console.log("Computer wins, paper beats rock!");
+    progressDiv.innerHTML =
+      "The computer's <b>PAPER</b> beats\nyour <b>ROCK</b>!\n\nComputer earns a point.";
     return computerScore++;
   } else if (computerInput === "scissors" && playerInput === "paper") {
-    console.log("Computer wins, scissors beats paper!");
+    progressDiv.innerHTML =
+      "The computer's <b>SCISSORS</b> beats\nyour <b>PAPER</b>!\n\nComputer earns a point.";
     return computerScore++;
   } else {
-    console.log("Draw, neither player nor computer wins.");
+    progressDiv.innerHTML = "<b>Draw</b>!\n\nPlease continue playing.";
   }
 }
 
 function startGame() {
+  welcomeDiv.innerHTML =
+    "Please choose\nRock, Paper, or Scissors.\n\n<b>First</b> to <b>5</b> is the winner.";
+
   rockBtn.addEventListener("click", () => {
-    console.log("Player chose rock.");
+    welcomeDiv.textContent = "";
     evaluateRound("rock", computerChoice(randomInt(1, 3)));
     displayResults();
   });
 
   paperBtn.addEventListener("click", () => {
-    console.log("Player chose paper.");
+    welcomeDiv.textContent = "";
     evaluateRound("paper", computerChoice(randomInt(1, 3)));
     displayResults();
   });
 
   scissorsBtn.addEventListener("click", () => {
-    console.log("Player chose scissors.");
+    welcomeDiv.textContent = "";
     evaluateRound("scissors", computerChoice(randomInt(1, 3)));
     displayResults();
   });
@@ -75,12 +84,16 @@ function displayResults() {
     rockBtn.disabled = true;
     paperBtn.disabled = true;
     scissorsBtn.disabled = true;
-    winnerDiv.textContent = "Player wins!";
+    progressDiv.textContent = "";
+    resultsDiv.textContent = "";
+    winnerDiv.textContent = "Player Wins!";
   } else if (computerScore == 5) {
     rockBtn.disabled = true;
     paperBtn.disabled = true;
     scissorsBtn.disabled = true;
-    winnerDiv.textContent = "Computer wins!";
+    progressDiv.textContent = "";
+    resultsDiv.textContent = "";
+    winnerDiv.textContent = "Computer Wins!";
   }
 }
 
